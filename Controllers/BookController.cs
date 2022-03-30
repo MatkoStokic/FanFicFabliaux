a49b51eaf0669ca24fb73abce77b499ef4f9f630
janzulovi <janzulovi@tvz.hr>
2022-03-30 21:44:28 +0200

    Database schema done

---
+++ Controllers/BookController.cs
@@ -0,0 +1,20 @@
+﻿using FanFicFabliaux.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FanFicFabliaux.Controllers
+{
+    public class BookController : Controller
+    {
+        private ApplicationDbContext dbContext;
+        public BookController(ApplicationDbContext dbContext) {
+            this.dbContext = dbContext;
+        }
+
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View(this.dbContext.Books);
+        }
+    }
+}
--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -11,15 +11,62 @@ namespace FanFicFabliaux.Data
         protected ApplicationDbContext(){}
 
         public DbSet<Book> Books { get; set; }
+        public DbSet<BookState> BookStates { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Subscription> Subscriptions { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<BookTag> BookTags { get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
 
-            builder.Entity<Book>().HasData(new Book { Id = 1, Title = "LOTR: Fellowship Of The Ring" });
-            builder.Entity<Book>().HasData(new Book { Id = 2, Title = "LOTR: Two Towers" });
-            builder.Entity<Book>().HasData(new Book { Id = 3, Title = "LOTR: The Return Of The King" });
+            builder
+                .Entity<BookTag>()
+                .HasOne(bt => bt.Book)
+                .WithMany(bt => bt.BookTags)
+                .HasForeignKey(bt => bt.BookId);
+
+            builder
+                .Entity<BookTag>()
+                .HasOne(bt => bt.Tag)
+                .WithMany(bt => bt.BookTags)
+                .HasForeignKey(bt => bt.TagId);
+
+            builder
+                .Entity<BookTag>()
+                .HasData(
+                    new BookTag { Id = 1, TagId = 1, BookId = 1},
+                    new BookTag { Id = 2, TagId = 1, BookId = 2 }, 
+                    new BookTag { Id = 3, TagId = 1, BookId = 3 }
+                );
+
+            builder
+                .Entity<Category>()
+                .HasData(
+                    new Category { Id = 1, CategoryName = "Fantasy"},
+                    new Category { Id = 2, CategoryName = "Mystery" },
+                    new Category { Id = 3, CategoryName = "Romance" },
+                    new Category { Id = 4, CategoryName = "Thriller" }
+                );
+
+            builder
+                .Entity<Tag>()
+                .HasData(
+                    new Tag { Id = 1, TagName = "LOTR" },
+                    new Tag { Id = 2, TagName = "tolkien" }
+                );
+
+            builder
+                .Entity<Book>()
+                .HasData(
+                new Book { Id = 1, Title = "LOTR: Fellowship Of The Ring", Author = "J.R.R. Tolkien", CategoryId = 1 },
+                new Book { Id = 2, Title = "LOTR: Two Towers", Author = "J.R.R. Tolkien", CategoryId = 1 },
+                new Book { Id = 3, Title = "LOTR: The Return Of The King", Author = "J.R.R. Tolkien", CategoryId = 1 }
+            );
+
         }
 
     }
