98f867fd2dc3143d8524edd7edaea7d1b7197f3b
DesBelli <stokic.matko@gmail.com>
2022-04-11 22:49:37 +0200

    Wrapped writeBook js in document ready

--- wwwroot/js/writeBook.js
+++ wwwroot/js/writeBook.js
@@ -1,21 +1,22 @@
-﻿const file = document.getElementById("Input_Datoteka");
-const text = document.getElementById("Input_Tekst");
+﻿$(document).ready(function () {
+    const file = document.getElementById("Input_Datoteka");
+    const text = document.getElementById("Input_Tekst");
 
-file.addEventListener('change', (event) => {
-    disableIfHasValue(event.target.value, text);
-})
+    file.addEventListener('change', (event) => {
+        disableIfHasValue(event.target.value, text);
+    })
 
-text.addEventListener('change', (event) => {
-    disableIfHasValue(event.target.value, file);
-})
+    text.addEventListener('change', (event) => {
+        disableIfHasValue(event.target.value, file);
+    })
 
-disableIfHasValue(text.value, file);
-disableIfHasValue(file.value, text);
+    disableIfHasValue(text.value, file);
+    disableIfHasValue(file.value, text);
 
-function disableIfHasValue(value, el) {
-    if (value === "") {
-        el.removeAttribute('disabled');
-    } else {
-        el.setAttribute('disabled', 'disabled');
-    }
-}
+    function disableIfHasValue(value, el) {
+        if (value === "") {
+            el.removeAttribute('disabled');
+        } else {
+            el.setAttribute('disabled', 'disabled');
+        }
+    }});
