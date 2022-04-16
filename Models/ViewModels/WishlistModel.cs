7c9cfb85008406c9bcba2626b614341be163d65c
janzulovi <janzulovi@tvz.hr>
2022-04-16 16:09:59 +0200

    Wishlist drugi put

---
+++ Models/ViewModels/WishlistModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FanFicFabliaux.Models.ViewModels
+{
+    public class WishlistModel
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Length { get; set; }
+        public string Category { get; set; }
+
+        [BindProperty]
+        public Book Book { get; set; }
+    }
+}
